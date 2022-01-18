11526a485917c8caaf62e7dc047f803a451097eb
seventty <elmastigeron2015@gmail.com>
2022-01-18 16:41:30 -0400

    update

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-// create an express app
 const express = require("express")
 const app = express()
 
-
 app.use(express.static("public"))
 
+app.get("/getData", function (req, res) {
+  res.send("<h1>Get endpoint working</h1>");
+  console.log("Get data");
+});
 
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+app.post("/postData", function (req, res) {
+  res.send("<h1>Post endpoint working</h1>");
+  console.log("Post data");
 })
 
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
