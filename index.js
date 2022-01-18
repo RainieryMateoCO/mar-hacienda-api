6350962538d1fe92b29c203b50c093920dbc67d7
seventty <elmastigeron2015@gmail.com>
2022-01-18 16:35:46 -0400

    update

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// create an express app
+const express = require("express")
+const app = express()
 
-app.get('/', (req, res) => res.send("Hello world"));
 
-app.listen(port, () => console.log("App listening at: ", port));
+app.use(express.static("public"))
+
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
